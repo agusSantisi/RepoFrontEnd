96fa7d49d587a568da76beb4ba6378a14561d360
agusSantisi <agustinargprograma@gmail.com>
2023-04-26 16:56:47 -0300

    Se agregó el Header y el banner principal

--- FrontEnd/PortfolioAngular/src/app/app.module.ts
+++ FrontEnd/PortfolioAngular/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { HysComponent } from './components/hys/hys.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { LoginComponent } from './components/login/login.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    HysComponent,
+    FooterComponent,
+    ProyectosComponent,
+    LoginComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
